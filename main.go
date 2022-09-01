4852fdb8fdb223591c49545a92cd843b5cedf19b
geolffreym <gmjun2000@gmail.com>
2022-09-01 08:32:45 -0600

    feat: graceful shutdown for docker stop

--- main.go
+++ main.go
@@ -78,8 +78,8 @@ func main() {
 
 	c := make(chan os.Signal, 1)
 	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt)
+	// SIGKILL, SIGQUIT or SIGTERM will be caught if server run inside docker.
+	signal.Notify(c, os.Kill, os.Interrupt)
 
 	// Block until we receive our signal.
 	<-c
